983d5ce1670473a9b1ba23b7b90b43027c7abbc8
LuigimonSoft <lccarrillot@gmail.com>
2024-06-17 14:37:31 -0400

    add the swagger configuration to the proyect

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,9 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import swaggerJsdoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
+
 
 import { errorHandler } from './middleware/errorHandler';
 
@@ -13,6 +16,27 @@ const port = process.env.PORT || 3000;
 const apiPrefix = process.env.API_PREFIX || '/api/v1';
 
 app.use(cors());
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Kumojin challenge code API',
+      version: '1.0.0',
+      description: 'A simple API to manage events.',
+    },
+    servers: [
+      {
+        url: `http://localhost:${port}`,
+      },
+    ],
+  },
+  apis: ['./src/routes/*.ts'],
+};
+
+const specs = swaggerJsdoc(options);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 
 app.use(bodyParser.json());
 
