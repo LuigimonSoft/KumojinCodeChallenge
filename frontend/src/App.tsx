e1dd70df3e716c0b76b004da476eceef7927557a
LuigimonSoft <lccarrillot@gmail.com>
2024-06-17 16:58:29 -0400

    add card to center the information

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,10 @@ import './App.css'
 function App() {
 
   return (
-    <div>
+    <div className="h-screen flex items-center justify-center bg-gray-100">
+      <div className="block max-w-xg p-6 bg-white border border-gray-200 rounded-lg shadow ">
+        
+      </div>
     </div>
   )
 }
