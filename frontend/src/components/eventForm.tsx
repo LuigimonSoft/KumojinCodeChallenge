00cb60fe35d7419bf93454c5d7cb817ce37b94f0
LuigimonSoft <lccarrillot@gmail.com>
2024-06-19 12:27:02 -0400

    update unit test

--- frontend/src/components/eventForm.tsx
+++ frontend/src/components/eventForm.tsx
@@ -58,15 +58,15 @@ const EventForm: React.FC<EventFormProps> = ({ event, onSave, onClose, isViewMod
   return (
     <div id="editEventModal" tabIndex={-1} aria-hidden="true" className="fixed top-0 left-0 right-0 z-50 items-center justify-center w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full flex" role="dialog">
       <div className="relative w-full max-w-2xl max-h-full">
-        <form onSubmit={handleSubmit} className="relative bg-white rounded-lg shadow ">
+        <form onSubmit={handleSubmit} className="relative bg-white rounded-lg shadow " data-testid="form">
 
           <div className="flex items-start justify-between p-4 border-b rounded-t">
             <h3 className="text-xl font-semibold text-gray-900 ">
               Event
             </h3>
-            <button type="button" onClick={handleClose} className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center " data-modal-hide="editEventModal">
+            <button type="button" data-testid="btnClose"  onClick={handleClose} className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center " data-modal-hide="editEventModal">
               <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
               </svg>
               <span className="sr-only">Close modal</span>
             </button>
--- frontend/src/components/eventTable.tsx
+++ frontend/src/components/eventTable.tsx
@@ -71,7 +71,6 @@ const EventTable: React.FC = () => {
         setEvents(event);
       else {
         setEvents([]);
-        setSearchError('No events found');
       }
     } catch (error) {
       setSearchError('Error searching for event');
@@ -96,18 +95,16 @@ const EventTable: React.FC = () => {
               <div className="relative mt-1">
                 <div className="absolute inset-y-0 rtl:inset-r-0 start-0 flex items-center ps-3 pointer-events-none">
                   <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
                   </svg>
                 </div>
-                <input type="text" id="table-search"  value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} className="block pt-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 "  placeholder="Search for name"/>
+                <input type="text" id="table-search" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} className="block pt-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 " placeholder="Search for name" />
               </div>
             </div>
             <button
               type="submit"
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-            >
-              Search
-            </button>
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" data-testid="btnSearch"
+            >Search</button>
             <button onClick={handleCreateNewEvent} className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
                 New Event
             </button>
